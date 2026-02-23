7fe0c3d0b9105a95df9dcf63e401071b12f75ecb
AlmanJangul <za13144@auca.kg>
2026-02-23 10:33:20 +0600

    I did

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class CampusLifePlanner {
+    public static void main(String[] args) {
+
+        // Create courses
+        Course oop = new Course("OOP", "Dr. Lee", 6);
+        Course math = new Course("Discrete Math", "Prof. Smith", 3);
+
+        // Create assignments
+        AssignmentTask lab2 = new AssignmentTask("Lab 2", oop, 3, 1);
+        AssignmentTask homework1 = new AssignmentTask("HW 1", math, 5, 5);
+
+        // Print courses
+        System.out.println(oop);
+        System.out.println(math);
+
+        System.out.println();
+
+        // Print assignments
+        System.out.println(lab2);
+        System.out.println("Is urgent? " + lab2.isUrgent());
+
+        System.out.println();
+
+        System.out.println(homework1);
+        System.out.println("Is urgent? " + homework1.isUrgent());
+
+        System.out.println();
+
+        // Mark Lab2 completed
+        lab2.markCompleted();
+        System.out.println("After completing Lab 2:");
+        System.out.println(lab2);
+        System.out.println("Is urgent? " + lab2.isUrgent());
+    }
+}
